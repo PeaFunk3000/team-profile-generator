ec38be3b7703216c4b0fb1ffe282f3546a1337e2
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-02-24 22:38:08 +0000

    inquirer manager prompts

--- index.js
+++ index.js
@@ -10,7 +10,42 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Name
+// Employee ID
+// Email address
+// Office number
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the Manager's name?",
+      name: "MrManager",
+    },
+    {
+      type: "number",
+      message: "What is the Manager's ID?",
+      name: "manID",
+    },
+    {
+      type: "input",
+      message: "What is the Manager's email?",
+      name: "manEmail",
+    },
+    {
+      type: "number",
+      message: "What is the Manager's office number?",
+      name: "manID",
+    },
+  ])
 
+  .then((response) => {
+    
+  });
 
+//   {
+//     type: "list",
+//     message: "What to do next?",
+//     name: "",
+//     choices: ["Finish Team", "Add Engineer", "Add Intern",],
+//   },
